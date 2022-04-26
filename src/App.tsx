a7a77ffee6f7393417930c81aeb99e16f63ba034
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2022-04-27 01:41:41 +0700

    Import components to main app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
+import { Navbar, FilteredBrand, Card } from "./components";
 import "./App.scss";
 
-const App = () => {
-	return <div className="App"></div>;
+const App: React.FC = () => {
+	const [brandSelected, setBrandSelected] = useState<string>("");
+
+	useEffect(() => {
+		// fetching new data after brand has been changed
+		console.log(brandSelected);
+	}, [brandSelected]);
+
+	return (
+		<>
+			<Navbar />
+			<div className="body__container">
+				<FilteredBrand
+					brandSelected={brandSelected}
+					setBrandSelected={setBrandSelected}
+				/>
+				<div className="grid__container">
+					<Card />
+					<Card />
+					<Card />
+					<Card />
+					<Card />
+					<Card />
+					<Card />
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default App;

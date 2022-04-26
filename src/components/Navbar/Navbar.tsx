671b7e36b3e2e1f48a4b3cc57a22931ac5315357
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2022-04-26 23:47:55 +0700

    Create navbar components

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./navbar.scss";
+
+const Navbar: React.FC = () => {
+	return (
+		<div className="navbar__container">
+			<div className="navbar__wrapper">
+				<h1>Car rental</h1>
+			</div>
+		</div>
+	);
+};
+
+export default Navbar;

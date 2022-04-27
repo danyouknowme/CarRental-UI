56c1e6d0d4907ca482fdee7afa8c1fa897470e16
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2022-04-27 19:04:53 +0700

    Add interface of user and car

--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,24 @@ export interface IFilteredBrand {
   brandSelected: string;
   setBrandSelected: (value: string) => void;
 }
+
+export interface IUser {
+  firstName: string;
+  lastName: string;
+  age: number;
+  phone: string;
+}
+
+export interface AccordionProps {
+  usersData: IUser[];
+}
+
+export interface ICar {
+  brand: string;
+  model: string;
+  price: number;
+}
+
+export interface TableProps {
+  carsData: ICar[];
+}

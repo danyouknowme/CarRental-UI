c5b7b5a88ce0e976b510d0c57307a6a8e56e9904
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2022-04-27 19:05:29 +0700

    Create accordion component

---
+++ src/components/Accordion/Accordion.tsx
@@ -0,0 +1,51 @@
+import * as React from "react";
+import {
+	Accordion,
+	AccordionDetails,
+	AccordionSummary,
+	Typography,
+} from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import "./accordion.scss";
+import { AccordionProps } from "../../types";
+
+const ControlledAccordions: React.FC<AccordionProps> = ({ usersData }) => {
+	const [expanded, setExpanded] = React.useState<string | false>(false);
+
+	const handleChange =
+		(panel: string) => (event: React.SyntheticEvent, isExpanded: boolean) => {
+			setExpanded(isExpanded ? panel : false);
+		};
+
+	return (
+		<div className="accordion__container">
+			{usersData.map((user, index) => (
+				<Accordion
+					expanded={expanded === `panel${index}`}
+					onChange={handleChange(`panel${index}`)}
+				>
+					<AccordionSummary
+						expandIcon={<ExpandMoreIcon />}
+						aria-controls="panel1bh-content"
+						id="panel1bh-header"
+					>
+						<Typography sx={{ width: "33%", flexShrink: 0 }}>
+							{user.firstName} {user.lastName}
+						</Typography>
+						<Typography sx={{ color: "text.secondary" }}>
+							Age: {user.age} Phone: {user.phone}
+						</Typography>
+					</AccordionSummary>
+					<AccordionDetails>
+						<Typography>
+							Nulla facilisi. Phasellus sollicitudin nulla et quam mattis
+							feugiat. Aliquam eget maximus est, id dignissim quam.
+						</Typography>
+					</AccordionDetails>
+				</Accordion>
+			))}
+		</div>
+	);
+};
+
+export default ControlledAccordions;

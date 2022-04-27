87f71146e7948502438b25b0c30a00de3bd4f1b1
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2022-04-27 19:05:12 +0700

    Create user page and display mock data

--- src/pages/User/User.tsx
+++ src/pages/User/User.tsx
@@ -1,7 +1,43 @@
 import React from "react";
+import { Navbar, Accordion } from "../../components";
+import "./user.scss";
 
 const User: React.FC = () => {
-	return <div>User</div>;
+	const usersData = [
+		{
+			firstName: "Thanathip",
+			lastName: "Suwannakhot",
+			age: 20,
+			phone: "0951683442",
+		},
+		{
+			firstName: "Sirawich",
+			lastName: "Tumtamai",
+			age: 21,
+			phone: "0951683442",
+		},
+		{ firstName: "Bhokin", lastName: "Tumtamai", age: 19, phone: "0951683442" },
+		{
+			firstName: "Poomtum",
+			lastName: "Tumtamai",
+			age: 22,
+			phone: "0951683442",
+		},
+	];
+
+	return (
+		<>
+			<Navbar />
+			<div className="users__container">
+				<div className="user__text">
+					<h1>Users</h1>
+				</div>
+				<div className="user_data__container">
+					<Accordion usersData={usersData} />
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default User;

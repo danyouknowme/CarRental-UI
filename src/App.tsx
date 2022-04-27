e2fc96bbd82061e6d0709553bb452ee6011a5a7a
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2022-04-27 19:04:26 +0700

    Add theme provider of mui

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,22 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.scss";
 import { Home, User } from "./pages";
+import { createTheme, ThemeProvider } from "@mui/material";
+
+const theme = createTheme({
+	typography: {
+		fontFamily: ["Inter", "sans-serif"].join(","),
+	},
+});
 
 const App: React.FC = () => {
 	return (
-		<Routes>
-			<Route path="/" element={<Home />}></Route>
-			<Route path="/users" element={<User />}></Route>
-		</Routes>
+		<ThemeProvider theme={theme}>
+			<Routes>
+				<Route path="/" element={<Home />}></Route>
+				<Route path="/users" element={<User />}></Route>
+			</Routes>
+		</ThemeProvider>
 	);
 };
 

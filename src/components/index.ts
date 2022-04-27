b914720f9d9aa2495fd2cf300d268028f44732c4
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2022-04-27 19:05:43 +0700

    Export default components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,5 @@
 export { default as Navbar } from "./Navbar/Navbar";
 export { default as FilteredBrand } from "./FilteredBrand/FilteredBrand";
 export { default as Card } from "./Card/Card";
+export { default as Accordion } from "./Accordion/Accordion";
+export { default as Table } from "./Table/Table";
